f2cb6f2c817f186bf3b8e5ecfb25433f575ca2c6
isha chopde <isha.chopde250794@gmail.com>
2017-10-13 16:20:24 -0700

    push to heroku

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 51397);
