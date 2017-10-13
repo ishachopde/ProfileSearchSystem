acdbbaae7b35c86803a1da79c81b74a5e9765b4d
isha chopde <isha.chopde250794@gmail.com>
2017-10-13 16:26:27 -0700

    changed server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 // server.js
 const express = require('express');
-const http = require('http');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-http.createServer(onRequest).listen(process.env.PORT || 6000)
+app.listen(process.env.PORT || 51397);
