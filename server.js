a96e5ad383aa1213470179283b2fc88f92e47145
isha chopde <isha.chopde250794@gmail.com>
2017-10-14 14:50:48 -0700

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/src'));
 // Start the app by listening on the default
 // Heroku port
 
