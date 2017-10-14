fd7a687e32543b0cfd889da1fa3527d977232212
isha chopde <isha.chopde250794@gmail.com>
2017-10-14 14:38:37 -0700

    routes removed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
 const express = require('express');
 const app = express();
-var videos = require('./routes/index');
-app.use('/vids', videos);
+//var videos = require('./routes/index');
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/'));
