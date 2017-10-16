e2e03a7ae9de72c9a7deca7bb785a42603121e95
isha chopde <isha.chopde250794@gmail.com>
2017-10-16 11:21:55 -0700

    json updated

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default
 // Heroku port
